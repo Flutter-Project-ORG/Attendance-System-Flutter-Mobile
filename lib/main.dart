e9529dc56fca51cff337c54da3994af6bc985f00
Osama <osama.assaf.o@gmail.com>
2022-11-02 20:20:58 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
         canvasColor: CustomColors.lightBgColor,
         colorScheme: ColorScheme.fromSwatch().copyWith(
