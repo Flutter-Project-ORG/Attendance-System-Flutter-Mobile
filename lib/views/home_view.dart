02d5310ce900a872356f20eef9368e0ffee4d6e8
Osama <osama.assaf.o@gmail.com>
2022-11-02 20:10:34 +0300

    commit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -5,6 +5,11 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+      ),
+    );
   }
 }
